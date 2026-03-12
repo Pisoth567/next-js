1031f62d8db36e0f766655eff1078f71e84cb871
Pisoth567 <pisoth887@gmail.com>
2026-03-12 11:13:05 +0700

    update added middleware

--- lib/store.ts
+++ lib/store.ts
@@ -1,18 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { CounterSlice } from './features/counter/counterSlice'
 import { CounterCart } from './features/counter/couterCardState'
+import { productApi } from './features/product/productApi'
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
         counter: CounterSlice.reducer,
-        cartCounter: CounterCart.reducer
+        cartCounter: CounterCart.reducer,
+        [productApi.reducerPath] : productApi.reducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware)
   })
 }
 
-// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
